04719b4a317862123a6a6ae0db8afeee17aed872
Abishek K S <abishekshaji85@gmail.com>
2025-03-01 12:34:23 +0530

    Removed ignored files from Git tracking

--- dist/frameworks/routes/auth.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const tsyringe_1 = require("tsyringe");
-const AuthController_1 = require("../../adapters/controllers/AuthController");
-const authRoutes = (0, express_1.Router)();
-const authController = tsyringe_1.container.resolve(AuthController_1.AuthController);
-authRoutes.post('/signup', authController.signup);
-exports.default = authRoutes;
